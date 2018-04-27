5c9a64b75890b0bde4c3cd81e1ea806a52f54fe5
luojinghui <luojinghui@xylink.com>
2018-04-27 17:48:42 +0800

    add code spliting

--- src/component/App.js
+++ src/component/App.js
@@ -4,6 +4,24 @@ import styles from '../assets/scss/App.scss';
 import Content from './Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      a: 1,
+      b: 2,
+      value: 0
+    }
+  }
+
+  onClickContent = async () => {
+    let module = await import('../tools/');
+
+    this.setState({
+      value: module.add(this.state.a, this.state.b)
+    })
+  };
+
   render() {
     return (
       <div>
@@ -13,8 +31,13 @@ class App extends Component {
             <h1 className={styles.App_title}>Welcome to React</h1>
           </header>
         </div>
-        <div>
+        <div onClick={this.onClickContent}>
           <Content />
+          <h1>
+            {
+              `${this.state.a} + ${this.state.b} = ${this.state.value}`
+            }
+          </h1>
         </div>
         <p className={styles.App_intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/tools/index.js
@@ -0,0 +1,11 @@
+/**
+ * @author Luojinghui & luojinghui424@gmail.com
+ * @date 2018/4/27
+ * @Description:
+ */
+
+const add = (a, b) => {
+  return a + b;
+};
+
+export { add };
